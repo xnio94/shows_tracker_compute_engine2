d5e94b3ad1c9ec448292c3dc9e123b941fca57a4
xnio94 <xnio94@gmail.com>
2023-08-18 22:56:23 +0100

    commit 3

--- scripts.sh
+++ scripts.sh
@@ -18,6 +18,8 @@ sudo supervisorctl stop pythonapp
 
 cd /opt/app
 sudo -u pythonapp git pull origin main
+sed -i "s|OPENAI_API_KEY_PLACEHOLDER|$OPENAI_API_KEY|g"  /opt/app/python-app.conf
+
 sudo /opt/app/env/bin/pip install -r /opt/app/requirements.txt
 
 

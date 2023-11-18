1f85c2ef3e1ce6136b7ef077b48def5e5372b832
xnio94 <xnio94@gmail.com>
2023-11-18 13:11:36 +0800

    commit 5

--- code/selinuim.py
+++ code/selinuim.py
@@ -23,15 +23,15 @@ def start_driver(headless):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('start-maximized')
-    # options.add_argument('--window-size=1280x1696')
-    options.add_argument("disable-infobars")
-    options.add_argument("--disable-extensions")
-    options.add_argument("--disable-software-rasterizer")
-    options.add_argument("--remote-debugging-port=9222")
+    options.add_argument('--window-size=1280x1696')
+    # options.add_argument('start-maximized')
+    # options.add_argument("disable-infobars")
+    # options.add_argument("--disable-extensions")
+    # options.add_argument("--disable-software-rasterizer")
+    # # options.add_argument("--remote-debugging-port=9222")
 
 
-# options.add_argument("--window-size=800,450")
+    # options.add_argument("--window-size=800,450")
     driver = webdriver.Chrome(options=options, executable_path="/usr/bin/chromedriver")
     return driver
 

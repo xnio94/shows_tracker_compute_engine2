22032e2cf3bc3302fbc18aa650e96a90fbe7f750
xnio94 <xnio94@gmail.com>
2023-11-18 13:05:03 +0800

    commit 5

--- code/selinuim.py
+++ code/selinuim.py
@@ -23,7 +23,11 @@ def start_driver(headless):
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-gpu')
-    options.add_argument('--window-size=1280x1696')
+    options.add_argument('start-maximized')
+    # options.add_argument('--window-size=1280x1696')
+    options.add_argument("disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-software-rasterizer")
 
 
 
